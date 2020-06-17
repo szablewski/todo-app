82ba50dc4574670d9fe9f90144f8ec03ee646d82
bszablewski <bartosz.szablewski123@wp.pl>
2020-06-17 23:40:33 +0200

    Added tests TaskGroup service.

--- src/test/java/bartosz/szablewski/todoapp/service/TaskGroupServiceTest.java
+++ src/test/java/bartosz/szablewski/todoapp/service/TaskGroupServiceTest.java
@@ -1,8 +1,11 @@
 package bartosz.szablewski.todoapp.service;
 
+import bartosz.szablewski.todoapp.repository.TaskGroupsRepository;
 import bartosz.szablewski.todoapp.repository.TaskRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -16,8 +19,7 @@ class TaskGroupServiceTest {
     @DisplayName("should throw when undone tasks")
     void toggleGroup_undoneTasks_throws_IllegalStateException() {
         //given
-        var mockTaskRepository = mock(TaskRepository.class);
-        when(mockTaskRepository.existsByDoneIsFalseAndGroups_Id(anyInt())).thenReturn(true);
+        TaskRepository mockTaskRepository = taskRepositoryReturning(true);
         // system under test
         var toTest = new TaskGroupService(null, mockTaskRepository);
         //when
@@ -26,5 +28,29 @@ class TaskGroupServiceTest {
         assertThat(exception)
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("undone task");
+    }
+
+    @Test
+    @DisplayName("should throw when no group")
+    void toggleGroup_wrongId_throwsIllegarArgumentException() {
+        //given
+        TaskRepository mockTaskRepository = taskRepositoryReturning(false);
+        //and
+        var mockRepository = mock(TaskGroupsRepository.class);
+        when(mockRepository.findById(anyInt())).thenReturn(Optional.empty());
+        //start under test
+        var toTest = new TaskGroupService(mockRepository, mockTaskRepository);
+        //when
+        var exception = catchThrowable(() -> toTest.toggleGroup(1));
+        //then
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("not found");
+    }
+
+    private TaskRepository taskRepositoryReturning(final boolean result) {
+        var mockTaskRepository = mock(TaskRepository.class);
+        when(mockTaskRepository.existsByDoneIsFalseAndGroups_Id(anyInt())).thenReturn(result);
+        return mockTaskRepository;
     }
 }
